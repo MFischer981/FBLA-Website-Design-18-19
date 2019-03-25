e0ea30265f189a11201a5db9fe1066f006683017
Chad Williams <42282637+CWilliams007@users.noreply.github.com>
2019-03-24 22:36:46 -0700

    ThankYou Modal
    Upon hitting submit on the contact form a thank you modal appears. cssParser.js and component.css are required files for its operation.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# FBLA-Website-Design-18-19
-FBLA Website Design 18-19
+# FBLA-Website-Design-Final
+FBLA Website Design Final
---
+++ main.js
@@ -0,0 +1,55 @@
+function showAnswer() {
+   event.target.parentNode.classList.toggle('center'); 
+   event.target.parentNode.childNodes[5].classList.toggle('show'); 
+   event.target.classList.toggle('click'); 
+}
+
+
+// contact us page, upon submit button being hit a Thank You modal will appear.
+var ModalEffects = (function() {
+
+	function init() {
+
+		var overlay = document.querySelector( '.md-overlay' );
+
+		[].slice.call( document.querySelectorAll( '.md-trigger' ) ).forEach( function( el, i ) {
+
+			var modal = document.querySelector( '#' + el.getAttribute( 'data-modal' ) ),
+				close = modal.querySelector( '.md-close' );
+
+			function removeModal( hasPerspective ) {
+				classie.remove( modal, 'md-show' );
+
+				if( hasPerspective ) {
+					classie.remove( document.documentElement, 'md-perspective' );
+				}
+			}
+
+			function removeModalHandler() {
+				removeModal( classie.has( el, 'md-setperspective' ) ); 
+			}
+
+			el.addEventListener( 'click', function( ev ) {
+				classie.add( modal, 'md-show' );
+				overlay.removeEventListener( 'click', removeModalHandler );
+				overlay.addEventListener( 'click', removeModalHandler );
+
+				if( classie.has( el, 'md-setperspective' ) ) {
+					setTimeout( function() {
+						classie.add( document.documentElement, 'md-perspective' );
+					}, 25 );
+				}
+			});
+
+			close.addEventListener( 'click', function( ev ) {
+				ev.stopPropagation();
+				removeModalHandler();
+			});
+
+		} );
+
+	}
+
+	init();
+
+})();
